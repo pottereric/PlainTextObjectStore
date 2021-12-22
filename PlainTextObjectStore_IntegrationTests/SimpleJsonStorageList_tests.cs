e396134b33d5bc00e98b79914445cf6e3e1f8032
Eric Potter <ft@mailinator.com>
2021-12-21 22:51:25 -0500

    move usings to a global file

--- PlainTextObjectStore_IntegrationTests/SimpleJsonStorageList_tests.cs
+++ PlainTextObjectStore_IntegrationTests/SimpleJsonStorageList_tests.cs
@@ -1,47 +1,42 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PlainTextObjectStore;
-using System.IO;
+namespace PlainTextObjectStore_IntegrationTests;
 
-namespace PlainTextObjectStore_IntegrationTests
+[TestClass]
+public class SimpleJsonStorageList_tests
 {
-    [TestClass]
-    public class SimpleJsonStorageList_tests
+    [TestMethod]
+    public void T1_CanCreateFiles()
     {
-        [TestMethod]
-        public void T1_CanCreateFiles()
-        {
-            var testReviewA = new CodeReview() { CreatorName = "Don Syme", ID = 7, ProjectName = "F#" };
-            var testReviewB = new CodeReview() { CreatorName = "Anders", ID = 7, ProjectName = "C#" };
-            var testReviewC = new CodeReview() { CreatorName = "Anders", ID = 7, ProjectName = "TypeScript" };
+        var testReviewA = new CodeReview() { CreatorName = "Don Syme", ID = 7, ProjectName = "F#" };
+        var testReviewB = new CodeReview() { CreatorName = "Anders", ID = 7, ProjectName = "C#" };
+        var testReviewC = new CodeReview() { CreatorName = "Anders", ID = 7, ProjectName = "TypeScript" };
 
-            var testList = new JsonBackedDictionary<CodeReview>("testObjects");
-            testList.Add(testReviewA);
-            testList.Add(testReviewB);
-            testList.Add(testReviewC);
+        var testList = new JsonBackedDictionary<CodeReview>("testObjects");
+        testList.Add(testReviewA);
+        testList.Add(testReviewB);
+        testList.Add(testReviewC);
 
-            var fileCount = Directory.GetFiles("testObjects").Length;
-            Assert.AreEqual(3, fileCount);
-        }
+        var fileCount = Directory.GetFiles("testObjects").Length;
+        Assert.AreEqual(3, fileCount);
+    }
 
-        [TestMethod]
-        public void T2_CanLoadFiles()
-        {
-            var testList = new JsonBackedDictionary<CodeReview>("testObjects");
+    [TestMethod]
+    public void T2_CanLoadFiles()
+    {
+        var testList = new JsonBackedDictionary<CodeReview>("testObjects");
 
-            var fileCount = Directory.GetFiles("testObjects").Length;
-            Assert.AreEqual(3, fileCount);
-        }
+        var fileCount = Directory.GetFiles("testObjects").Length;
+        Assert.AreEqual(3, fileCount);
+    }
 
-        [TestMethod]
-        public void T3_CanClearFiles()
-        {
-            var testList = new JsonBackedDictionary<CodeReview>("testObjects");
+    [TestMethod]
+    public void T3_CanClearFiles()
+    {
+        var testList = new JsonBackedDictionary<CodeReview>("testObjects");
 
-            testList.Clear();
+        testList.Clear();
 
-            var fileCount = Directory.GetFiles("testObjects").Length;
-            Assert.AreEqual(0, fileCount);
+        var fileCount = Directory.GetFiles("testObjects").Length;
+        Assert.AreEqual(0, fileCount);
 
-        }
     }
 }
